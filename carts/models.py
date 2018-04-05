f621edca3edbc85c42fcef5cce485167a5683497
Bruce Li <henninglee2013@gmail.com>
2018-04-06 00:06:43 +1000

    cart debug

--- carts/models.py
+++ carts/models.py
@@ -10,6 +10,8 @@ User = settings.AUTH_USER_MODEL
 class CartManager(models.Manager):
 	def new_or_get(self, request):
 		cart_id =request.session.get("cart_id", None)
+		print('1.{cart_id}'.format(cart_id=cart_id))
+		print(request.session.items())
 		qs = self.get_queryset().filter(id=cart_id)
 		if qs.count() == 1:
 			new_object = False
@@ -21,7 +23,10 @@ class CartManager(models.Manager):
 		else:
 			cart_obj = Cart.objects.new(user=request.user)
 			new_object = True
-			request.session['cart_ id'] = cart_obj.id
+			request.session['cart_id'] = cart_obj.id
+			print('2.{cart_id}'.format(cart_id=request.session['cart_id']))
+			print(request.session.items())
+
 		return cart_obj, new_object
 
 	def new(self, user=None):
--- carts/views.py
+++ carts/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse 
 from .models import Cart
 from products.models import Product
 
 
+
 def cart_page(request):
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
-	return render(request, "carts/home.html", {})
+	return render(request, "carts/home.html", {'carts':cart_obj})
 
 
 def cart_update(request):
-	print(request)
-	product = Product.objects.get(id=2)
+	product_id = request.POST.get('product_id')
+	product = Product.objects.get(id=product_id)
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
 	if cart_obj is not None:
 		if product in cart_obj.products.all():
 			cart_obj.products.remove(product)
 		else:
 			cart_obj.products.add(product)
-	return render(request, "carts/home.html", {})
+	return redirect(reverse('products:detail', kwargs={'slug':product.slug}))
