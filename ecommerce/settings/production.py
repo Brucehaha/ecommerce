8a25dea26de061f6dbb24f6f2376e65f74bd6aca
brucehaha <henninglee2013@gmail.com>
2018-06-21 16:57:01 +1000

    add from ecommerce.aws.conf import*

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -153,6 +153,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 MEDIA_URL = '/meida/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
+from ecommerce.aws.conf import *
 
 # https://kirr.co/vklau5
 
