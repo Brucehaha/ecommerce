51481303db61f4ddd46e74cd80c851d9b2b2e318
brucehaha <henninglee2013@gmail.com>
2018-07-11 17:14:45 +1000

    modify account password function“

--- accounts/passwords/urls.py
+++ accounts/passwords/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('password_change/', auth_views.PasswordChangeView.as_view() ,name='change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view() ,name='password_change_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view() ,name='password_reset'),
-    re_path('reset/<uidb64>/<token>/', auth_views.PasswordResetDoneView.as_view() ,name='password_reset_confirm'),
-    path('password_reset/done/', auth_views.PasswordResetConfirmView.as_view() ,name='password_reset_done'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view() ,name='password_reset_complete'),
+    path('password/change/', auth_views.PasswordChangeView.as_view() ,name='change'),
+    path('password/change/done/', auth_views.PasswordChangeDoneView.as_view() ,name='password_change_done'),
+    path('password/reset/', auth_views.PasswordResetView.as_view() ,name='password_reset'),
+    re_path(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view() ,name='password_reset_confirm'),
+    path('password/reset/done/', auth_views.PasswordResetDoneView.as_view() ,name='password_reset_done'),
+    path('password/reset/complete/', auth_views.PasswordResetCompleteView.as_view() ,name='password_reset_complete'),
 
 ]
 
