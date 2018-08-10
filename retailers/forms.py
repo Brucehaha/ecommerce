aa2a3eaf52d5699bc87add9040d1c39fd84bc737
brucehaha <henninglee2013@gmail.com>
2018-08-10 17:57:48 +1000

    create and edit retailer

--- retailers/forms.py
+++ retailers/forms.py
@@ -1,26 +1,44 @@
 from django import forms
 from django.forms import formset_factory
-from .model import Retailer, SampleBridge
+from .models import Retailer, SampleBridge
 
 class RetailerForm(forms.ModelForm):
     class Meta:
         model = Retailer
+        exclude = [
+            'is_active',
+            'sample'
+        ]
+    def __init__(self, *args, **kwargs):
+        super(RetailerForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control input-sm'
 
 class SampleBridgeForm(forms.ModelForm):
     class Meta:
         model = SampleBridge
-
+        fields = [
+            'sample',
+            'number',
+        ]
+    def __init__(self, *args, **kwargs):
+        super(SampleBridgeForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control input-sm'
 
 SampleBridgeFormSet = forms.modelformset_factory(
     SampleBridge,
     form = SampleBridgeForm,
+    fields=('sample', 'number'),
     extra=2,
 )
 
 SampleBridgeInlineFormSet = forms.inlineformset_factory(
     Retailer,
     SampleBridge,
-    extra=2,
     formset=SampleBridgeFormSet,
+    fields=('sample', 'number'),
     min_num=1,
+    extra=2,
+
 )
--- retailers/views.py
+++ retailers/views.py
@@ -1,12 +1,123 @@
-from django.shortcuts import render
-from .models import Retailer
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.views.generic import ListView
-from .forms import RetailerForm, SampleBridgeForm
+from . import forms
+from . import models
+
 
 class RetailerListView(ListView):
-    model = Retailer
+    model = models.Retailer
     template_name = "retailers/retailer_list.html"
 
 
 def create_retailer(request):
-    form = RetailerForm
+    form_class = forms.RetailerForm
+    sample_forms = forms.SampleBridgeFormSet(
+        queryset = models.SampleBridge.objects.none()
+    )
+    form = form_class()
+    if request.method == 'POST':
+        form = form_class(request.POST)
+        sample_forms = forms.SampleBridgeFormSet(
+            request.POST,
+            queryset = models.SampleBridge.objects.none()
+        )
+
+    if form.is_valid() and sample_forms.is_valid():
+        retailer = form.save()
+        samples = sample_forms.save(commit=False)
+        for sample in samples:
+            sample.retailer = retailer
+            sample.save()
+            messages.success(request, "Added Retailer")
+            return redirect("retailers:list")
+    return render(request, 'retailers/retailer_form.html', {
+        'form': form,
+        'formset':sample_forms
+        })
+
+
+def create_retailer(request):
+    form_class = forms.RetailerForm
+    sample_forms = forms.SampleBridgeFormSet(
+        queryset = models.SampleBridge.objects.none()
+    )
+    form = form_class()
+    if request.method == 'POST':
+        form = form_class(request.POST)
+        sample_forms = forms.SampleBridgeFormSet(
+            request.POST,
+            queryset = models.SampleBridge.objects.none()
+        )
+
+    if form.is_valid() and sample_forms.is_valid():
+        retailer = form.save()
+        samples = sample_forms.save(commit=False)
+        for sample in samples:
+            sample.retailer = retailer
+            sample.save()
+            messages.success(request, "Added Retailer")
+            return redirect("retailers:list")
+    return render(request, 'retailers/retailer_form.html', {
+        'form': form,
+        'formset':sample_forms
+        })
+
+
+def create_retailer(request):
+    form_class = forms.RetailerForm
+    sample_forms = forms.SampleBridgeInlineFormSet(
+        queryset = models.SampleBridge.objects.none()
+    )
+    form = form_class()
+    if request.method == 'POST':
+        form = form_class(request.POST)
+        sample_forms = forms.SampleBridgeInlineFormSet(
+            request.POST,
+            queryset = models.SampleBridge.objects.none()
+        )
+
+    if form.is_valid() and sample_forms.is_valid():
+        retailer = form.save()
+        samples = sample_forms.save(commit=False)
+        for sample in samples:
+            sample.retailer = retailer
+            sample.save()
+            messages.success(request, "Added Retailer")
+            return redirect("retailers:list")
+    return render(request, 'retailers/retailer_form.html', {
+        'form': form,
+        'formset':sample_forms
+        })
+
+
+def edit_retailer(request, retailer_pk):
+    retailer = get_object_or_404(models.Retailer, pk=retailer_pk)
+    form_class = forms.RetailerForm
+
+    form = form_class(instance=retailer)
+    sample_forms = forms.SampleBridgeInlineFormSet(
+        queryset = form.instance.samplebridge_set.all()
+    )
+
+    if request.method == 'POST':
+        form = form_class(request.POST, instance=retailer)
+        sample_forms = forms.SampleBridgeInlineFormSet(
+            request.POST,
+            queryset =form.instance.samplebridge_set.all()
+        )
+
+    if form.is_valid() and sample_forms.is_valid():
+        form.save()
+        samples = sample_forms.save(commit=False)
+        for sample in samples:
+            sample.retailer = retailer
+            sample.save()
+        for sample in sample_forms.deleted_objects:
+            answer.delete()
+        messages.success(request, "Added Retailer")
+        return redirect("retailers:list")
+    return render(request, 'retailers/retailer_form.html', {
+        'form': form,
+        'formset':sample_forms
+        })
