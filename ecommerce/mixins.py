49482877972a4336e064de0d4a3f3b5ebfc66053
brucehaha <henninglee2013@gmail.com>
2018-08-23 16:27:36 +1000

    sample.xml

--- ecommerce/mixins.py
+++ ecommerce/mixins.py
@@ -1,7 +1,6 @@
 
 from django.utils.http import is_safe_url
-from django import forms
-from django.contrib import messages
+
 
 
 
@@ -22,43 +21,3 @@ class NextUrlMixin(object):
         if is_safe_url(redirect_path, request.get_host()):
                 return redirect_path
         return self.default_next
-
-'''inherit form BaseModelFormSet
- for clean the dupliated model formset values
- like:
- SampleBridgeFormSet = forms.modelformset_factory(
-     SampleBridge,
-     form = SampleBridgeForm,
-     formset=UniqueFieldFormSet,#default BaseModelFormSet
-     fields=('sample', 'number'),
-     extra=2,
- )'''
-
-class UniqueFieldFormSet(forms.BaseModelFormSet):
-    def __init__(self, *args, **kwargs):
-        self.request =  kwargs.pop('request', None)
-        # get field value from view modelformset_factory(field_value='sample',
-        #)
-        self.field_value=kwargs.pop('field_value', None)
-
-        super(UniqueFieldFormSet, self).__init__(*args, **kwargs)
-
-    def clean(self):
-        super(UniqueFieldFormSet, self).clean()
-        values = set()
-        field_value = self.field_value
-        # to get the form.instance.field function
-        func = getattr(self, 'form.instance.{}'.format(field_value),"none")
-        for form in self.forms:
-            #to skip the empty form coz there's extra form for
-            # modelformset_factory
-            try:
-                value = form.cleaned_data[field_value]
-            except KeyError:
-                break
-            form.cleaned_data[field_value] = value
-            func = value
-            if value in values:
-                messages.success(self.request, 'duplicated "%s"' % value)
-                raise forms.ValidationError('Duplicate values for "%s" are not allowed.' % value)
-            values.add(value)
--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -8,37 +8,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-import re
 # from import_env import read_env
 from django.core.wsgi import get_wsgi_application
 
 '''set credential before application run on server, because applicaion do not
 run by manage.py anymore on server.
 '''
-def read_env(path):
-    """Pulled from Honcho code with minor updates, reads local default
-    environment variables from a .env file located in the project root
-    directory.
-    """
-    try:
-        with open(path) as f:
-            content = f.read()
-    except IOError:
-        content = ''
-
-    for line in content.splitlines():
-        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
-        if m1:
-            key, val = m1.group(1), m1.group(2)
-            m2 = re.match(r"\A'(.*)'\Z", val)
-            if m2:
-                val = m2.group(1)
-            m3 = re.match(r'\A"(.*)"\Z', val)
-            if m3:
-                val = re.sub(r'\\(.)', r'\1', m3.group(1))
-            os.environ.setdefault(key, val)
-read_env('.env')
+# read_env('.env')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
 
 application = get_wsgi_application()
